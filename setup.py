03c6befd7a854160d311009df38c50236f0b9869
ryanraba <36732257+ryanraba@users.noreply.github.com>
2019-10-15 12:12:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='conversion',
+   version='0.1',
+   description='convert between data formats',
+   packages=['conversion'],
+   install_requires=['numpy'],
+)
