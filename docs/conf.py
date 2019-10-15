00cfd27a9b97353c00eada3dab7e4e1a2caa4299
ryanraba <36732257+ryanraba@users.noreply.github.com>
2019-10-15 14:43:25 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,21 +12,21 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = u'cngi_prototype'
+project = u'CASA Next Generation Infrastructure'
 copyright = u'2019, Ryan Raba'
 author = u'Ryan Raba'
 
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u''
+release = u'0.1b'
 
 
 # -- General configuration ---------------------------------------------------
@@ -40,6 +40,9 @@ release = u''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -67,7 +70,7 @@ language = None
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -75,7 +78,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,7 +105,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'cngi_prototypedoc'
+htmlhelp_basename = 'CASANextGenerationInfrastructuredoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -129,7 +132,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'cngi_prototype.tex', u'cngi\\_prototype Documentation',
+    (master_doc, 'CASANextGenerationInfrastructure.tex', u'CASA Next Generation Infrastructure Documentation',
      u'Ryan Raba', 'manual'),
 ]
 
@@ -139,7 +142,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'cngi_prototype', u'cngi_prototype Documentation',
+    (master_doc, 'casanextgenerationinfrastructure', u'CASA Next Generation Infrastructure Documentation',
      [author], 1)
 ]
 
@@ -150,8 +153,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'cngi_prototype', u'cngi_prototype Documentation',
-     author, 'cngi_prototype', 'One line description of project.',
+    (master_doc, 'CASANextGenerationInfrastructure', u'CASA Next Generation Infrastructure Documentation',
+     author, 'CASANextGenerationInfrastructure', 'One line description of project.',
      'Miscellaneous'),
 ]
 
