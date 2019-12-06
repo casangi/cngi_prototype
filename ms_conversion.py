63c55a079767990a47e13259bbbaffe9f8b31440
Ryan Raba <rraba@ullr.cv.nrao.edu>
2019-12-06 16:46:07 -0500

    removed

--- ms_conversion.py
+++
@@ -1,8 +0,0 @@
-from cngi.direct import InitializeFramework
-from cngi.conversion import ms_to_pq
-
-
-if __name__ == '__main__':
-  #client = InitializeFramework(workers=4, memory='8GB')
-  
-  ms_to_pq('/export/data_1/data/uid___A002_Xc3032e_X27c3.ms', ddi=25)
