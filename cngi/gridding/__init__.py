e408dfafa5e293c26c9f82e9bae85d031d0f50d3
Jan-Willem Steeb <jsteeb@nrao.edu>
2020-01-24 13:38:57 -0500

    Add function that calculates cell size and image size for gridding.

--- cngi/gridding/__init__.py
+++ cngi/gridding/__init__.py
@@ -6,3 +6,4 @@
 from .standard_gridder import *
 from .serial_gridder import serial_grid_dask, serial_grid_dask_sparse, serial_grid, serial_grid_psf
 from .dirty_image import dirty_image
+from .calc_image_cell_size import calc_image_cell_size
