9de5833b5227859b9b128468f7a41a51de7d7a2a
Ryan Raba <ryanraba@gmail.com>
2020-03-19 16:45:54 -0400

    cleanup

--- cngi/vis/chanaverage.py
+++ cngi/vis/chanaverage.py
@@ -16,7 +16,7 @@
 ########################
 def chanaverage(xds, width=1):
     """
-    Average data across channels
+    Average data across the channel axis
 
     Parameters
     ----------
@@ -30,27 +30,23 @@ def chanaverage(xds, width=1):
     xarray.core.dataset.Dataset
         New Visibility Dataset
     """
-    import xarray
-
+    
     # save names of coordinates, then reset them all to variables
     coords = [cc for cc in list(xds.coords) if cc not in xds.dims]
     xds = xds.reset_coords()
 
     # use remaining non-chan coordinates and attributes to initialize new return xds
     new_xds = xds[[cc for cc in list(xds.coords) if cc not in ['chan']]]
-
-    # find all variables with chan dimension (vwcd)
-    vwcds = [dv for dv in xds.data_vars if 'chan' in xds[dv].dims]
     
     for dv in xds.data_vars:
         xda = xds.data_vars[dv]
         
         # apply chan averaging to compatible variables
-        if dv in vwcds:
-            if (dv == 'DATA') and ('SIGMA_SPECTRUM' in vwcds):
+        if 'chan' in xda.dims:
+            if (dv == 'DATA') and ('SIGMA_SPECTRUM' in xds.data_vars):
                 xda = (xds.DATA / xds.SIGMA_SPECTRUM**2).coarsen(chan=width, boundary='trim').sum()
                 xda = xda * (xds.SIGMA_SPECTRUM**2).coarsen(chan=width, boundary='trim').sum()
-            elif (dv == 'CORRECTED_DATA') and ('WEIGHT_SPECTRUM' in vwcds):
+            elif (dv == 'CORRECTED_DATA') and ('WEIGHT_SPECTRUM' in xds.data_vars):
                 xda = (xds.CORRECTED_DATA * xds.WEIGHT_SPECTRUM).coarsen(chan=width, boundary='trim').sum()
                 xda = xda / xds.WEIGHT_SPECTRUM.coarsen(chan=width, boundary='trim').sum()
             else:
