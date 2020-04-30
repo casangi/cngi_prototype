287a62c4b9702ed42fceed4286aa55eedd567f73
Ryan Raba <ryanraba@gmail.com>
2020-04-30 13:16:22 -0400

    handle commonbeam and restoringbeam attrs

--- cngi/image/gaussianbeam.py
+++ cngi/image/gaussianbeam.py
@@ -13,17 +13,17 @@
 #   limitations under the License.
 
 ########################
-def gaussianbeam(xds, source='perplanebeams', scale=1.0, name='BEAM'):
+def gaussianbeam(xds, source='commonbeam', scale=1.0, name='BEAM'):
     """
-    Construct a gaussian beam of image dimensions from specified size or per-plane beam parameters
-
+    Construct a gaussian beam of image dimensions from specified size or beam attribute
+    
     Parameters
     ----------
     xds : xarray.core.dataset.Dataset
         input Visibility Dataset
-    source : str or tuple of floats
-        Source xds attr name to find the per-plane beam information, or a tuple describing the major axis, minor axis, and position angle
-        of the desired gaussian in (arcsec, arcsec, degrees), for example (1., 1., 30.).  Default is 'perplanebeams'
+    source : str or list of floats
+        Source xds attr name to find the beam information, or a list describing the major axis, minor axis, and position angle
+        of the desired gaussian in (arcsec, arcsec, degrees), for example [1., 1., 30.].  Default is 'commonbeam'
     scale : float
         peak amplitude of beam. Default is unity (1.0)
     name : str
@@ -36,46 +36,38 @@ def gaussianbeam(xds, source='perplanebeams', scale=1.0, name='BEAM'):
     """
     import xarray
     import dask.array as da
+    import numpy as np
     import cngi._helper.beams as chb
     
     # build beam from specified shape
-    if type(source) is tuple:
+    if not isinstance(source, str):
         beam = scale * chb.synthesizedbeam(source[0], source[1], source[2], len(xds.d0), len(xds.d1), xds.incr[:2])[0]
         beam_xda = xarray.DataArray(da.from_array(beam), dims=['d0', 'd1'], name=name)
+        beams = np.array(source)
         
     # otherwise put it together from attributes section
     else:
-        # note that attributes are just normal python lists, so this is not parallelized
-        # parse per-plane beams in to a dictionary of parameters
-        beams = {}
-        for line in xds.attrs[source]:
-            if not line[0].startswith('beam'): continue
-            lp = line[0].split('.')
-            if lp[1] not in beams: beams[lp[1]] = {}  # add channel to dict
-            if lp[2] not in beams[lp[1]]: beams[lp[1]][lp[2]] = {}  # add pol dict
-            beams[lp[1]][lp[2]][lp[3]+'.'+lp[4]] = line[1]
+        beams = np.array(xds.attrs[source])
         
-        # populate output xarray
-        chan_list = []
-        for cc in beams.keys():
-            pol_list = []
-            for pp in beams[cc].keys():
-                beam = beams[cc][pp]
-                chan, pol = int(cc[-1]), int(pp[-1])
-                if (beam['major.unit'] != 'arcsec') or (beam['minor.unit'] != 'arcsec') or (beam['positionangle.unit'] != 'deg'):
-                    print('ERROR: buildbeam unit conversion needed')
-                
-                np_beam = scale*chb.synthesizedbeam(beam['major.value'],
-                                                    beam['minor.value'],
-                                                    beam['positionangle.value'],
+        if beams.ndim == 1:
+            np_beam = scale * chb.synthesizedbeam(beams[0], beams[1], beams[2], len(xds.d0), len(xds.d1), xds.incr[:2])[0]
+            da_beam = da.from_array(np_beam, chunks=[xds.chunks['d0'][0], xds.chunks['d1'][0]])
+            beam_xda = xarray.DataArray(da_beam, dims=['d0','d1'])
+            
+        else:
+            chan_list = []
+            for cc in range(beams.shape[0]):
+                pol_list = []
+                for pp in range(beams.shape[1]):
+                    np_beam = scale*chb.synthesizedbeam(beams[cc][pp][0], beams[cc][pp][1], beams[cc][pp][2],
                                                     len(xds.d0), len(xds.d1), xds.incr[:2])[0][:,:,None,None]
+                    da_beam = da.from_array(np_beam, chunks=[xds.chunks['d0'][0], xds.chunks['d1'][0],
+                                                             xds.chunks['chan'][0], xds.chunks['pol'][0]])
+                    pol_list += [xarray.DataArray(da_beam, dims=['d0','d1','chan','pol']).assign_coords(
+                                                                                {'chan':[xds.chan[cc]], 'pol':[xds.pol[pp]]})]
                 
-                da_beam = da.from_array(np_beam, chunks=[xds.chunks['d0'][0], xds.chunks['d1'][0], xds.chunks['chan'][0], xds.chunks['pol'][0]])
-                pol_list += [xarray.DataArray(da_beam, dims=['d0','d1','chan','pol']).assign_coords(
-                                                                            {'chan':[xds.chan[chan]], 'pol':[xds.pol[pol]]})]
-                
-            chan_list += [xarray.concat(pol_list, dim='pol')]
+                chan_list += [xarray.concat(pol_list, dim='pol')]
         
-        beam_xda = xarray.concat(chan_list, dim='chan')
+            beam_xda = xarray.concat(chan_list, dim='chan')
     
-    return xds.assign({name:beam_xda})
+    return xds.assign({name:beam_xda}).assign_attrs({name+'_params':beams.tolist()})
