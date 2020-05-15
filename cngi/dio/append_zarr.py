ccc7e80089e5da52bbe02173a28721fa4ae99e6b
Jan-Willem <jwsteeb@gmail.com>
2020-05-15 18:10:06 -0400

    Expand append_zarr and write_zarr dunctionality (specify chunking). Simplify append_zarr interface.

--- cngi/dio/append_zarr.py
+++ cngi/dio/append_zarr.py
@@ -14,29 +14,22 @@
 
 
 #############################################
-def append_zarr(dataset,outfile,list_dask_array,list_data_variable_name,list_array_dimensions,graph_name='append_zarr'):
+def append_zarr(list_xarray_data_variables,outfile,chunks_return={},graph_name='append_zarr'):
     """
     Append a list of dask arrays to a zarr file on disk. If a data variable with the same name is found it will be overwritten.
     Data will probably be corrupted if append_zarr overwrites the data variable from which the dask array gets its data.
-    The xarray data variables must have dimensions that are a subset of the dimensions that are on disk. The number of elements in each list (list_dask_array,list_data_variable_name,list_array_dimensions) must be the same.
-    An example of parameters for saving two dask arrays:
-    list_data_variable_name = [psf_nat, sum_weight]
-    list_data_variable_name = ['PSF_NAT', 'SUM_WEIGHT']
-    list_array_dimensions = [['d0', 'd1', 'chan', 'pol'],['chan', 'pol']]
-    Note that all the dimensions in list_array_dimensions must already be in the dataset on disk. Any graphs that are not part of the list_dask_array will not be executed.
+    The xarray data variables in list_xarray_data_variables must have dimensions that are a subset of the dimensions that are on disk.
+    Any graphs that are not part of the list_dask_array will not be executed.
     
     Parameters
     ----------
-    dataset : xarray.core.dataset.Dataset
-        Dataset that the list_dask_array will be appended to.
+    list_xarray_data_variables: list of dask arrays
+        List of xarray datavariables to append.
     outfile : str
         The file name of the dataset on disk, generally ends in .zarr
-    list_dask_array: list of dask arrays
-        List of dask arrays to append.
-    list_data_variable_name: list of str
-        List of names that will be used to identify the list_dask_array in the dataset.
-    list_array_dimensions: list of (list of str)
-        A list where each element is a list of the dimensions for each array in list_dask_array.
+    chunks_return : dict of int
+        A dictionary with the chunk size that will be returned. For example {'time': 20, 'chan': 6}.
+        If chunks_return is not specified the chunking on disk will be used.
     graph_name : string
         The time taken to execute the graph and save the dataset is measured and saved as an attribute in the zarr file.
         The graph_name is the label for this timing information.
@@ -54,29 +47,40 @@ def append_zarr(dataset,outfile,list_dask_array,list_data_variable_name,list_arr
     import time
     
     start = time.time()
-    n_arrays = len(list_dask_array)
-    disk_dataset = xr.open_zarr(outfile)
+    n_arrays = len(list_xarray_data_variables)
+    try:
+        disk_dataset = xr.open_zarr(outfile)
+    except ValueError:
+        print("######### ERROR: Could not open " + outfile)
+    
+    print(list_xarray_data_variables[0].dims)
+    
     
     #Create a list of delayed zarr.create commands for each dask array in ist_dask_array.
     list_target_zarr = []
+    list_dask_array = []
     for i in range(n_arrays):
         #Create list of dimension chunk sizes on disk
         chunksize_on_disk =[]
-        for array_dim in list_array_dimensions[i]:
+        for array_dim in list_xarray_data_variables[i].dims:
             chunksize_on_disk.append(disk_dataset.chunks[array_dim][0])
         
+        
         #Rechunk the dask arrays to match the chunking on disk
-        dask_array = list_dask_array[i].rechunk(chunksize_on_disk[i])
+        #dask_array = list_dask_array[i].rechunk(chunksize_on_disk[i])
+        dask_array = list_xarray_data_variables[i].data.rechunk(chunksize_on_disk[i])
+        list_dask_array.append(dask_array)
         
         #Create list of delayed objects
-        mapper = get_mapper(outfile+'/'+list_data_variable_name[i])
+        mapper = get_mapper(outfile+'/'+ list_xarray_data_variables[i].name)
         list_target_zarr.append(dask.delayed(zarr.create)(
              shape=dask_array.shape,
              chunks=chunksize_on_disk,
              dtype=dask_array.dtype,
              store=mapper,
              overwrite=True)) #Can not specify the zarr file attributes at creation , attrs={'_ARRAY_DIMENSIONS':array_dimensions[i]} (last checked on May 2020 )
-             
+        
+    
     #Trigger compute of delayed zarr.create funmctions in list_target_zarr.
     da.store(list_dask_array,list_target_zarr,compute=True,flush=True)
     
@@ -84,19 +88,16 @@ def append_zarr(dataset,outfile,list_dask_array,list_data_variable_name,list_arr
     dataset_group = zarr.open_group(outfile,mode='a')
     #This should one day be done during zarr.create. See https://github.com/zarr-developers/zarr-python/issues/538.
     for i in range(n_arrays):
-        dataset_group[list_data_variable_name[i]].attrs['_ARRAY_DIMENSIONS'] = list_array_dimensions[i]
+        dataset_group[list_xarray_data_variables[i].name].attrs['_ARRAY_DIMENSIONS'] = list_xarray_data_variables[i].dims
     
     time_to_calc_and_store = time.time() - start
     print('Time to append and execute graph ', graph_name, time_to_calc_and_store)
     dataset_group.attrs[graph_name+'_time'] = time_to_calc_and_store
     
-    #Consolidate metadata
+    #Consolidate metadata #Can be improved by only adding appended metadata
     zarr.consolidate_metadata(outfile)
     
-    # Open dataset from zarr array preserving the chunking in dataset
-    # Must be a beter way to get dict from SortedKeyDict
-    current_dataset_chunk_size = {}
-    for dim_key in dataset.chunks:
-        current_dataset_chunk_size[dim_key] = dataset.chunks[dim_key][0]
-    
-    return xr.open_zarr(outfile,chunks=current_dataset_chunk_size,overwrite_encoded_chunks=True,consolidated=True)
+    if chunks_return is {}:
+        return xr.open_zarr(outfile,overwrite_encoded_chunks=True,consolidated=True)
+    else:
+        return xr.open_zarr(outfile,chunks=chunks_return,overwrite_encoded_chunks=True,consolidated=True)
