7db7d395990a446419462d9356bfdb71fa886a85
Benjamin Bean <bbean@nrao.edu>
2020-12-03 14:56:45 -0700

    maybe better formatting for ddijoin

--- cngi/vis/ddijoin.py
+++ cngi/vis/ddijoin.py
@@ -18,9 +18,8 @@ this module will be included in the api
 ########################
 def ddijoin(xds1, xds2):
     """
-    --todo: implement, still in development
-
     Concatenate together two Visibility Datasets of compatible shape
+
 
     Extended Summary
     ----------------
@@ -35,6 +34,7 @@ def ddijoin(xds1, xds2):
     from the first dataset override those from the second where the keys are the
     same.
 
+
     Parameters
     ----------
     xds1 : xarray.core.dataset.Dataset
@@ -42,17 +42,21 @@ def ddijoin(xds1, xds2):
     xds2 : xarray.core.dataset.Dataset
         second Visibility Dataset to join
 
+
     Returns
     -------
     xarray.core.dataset.Dataset
         New Visibility Dataset with combined contents
-    
+
+
     Warnings
     --------
     Joins are highly discouraged for datasets that don't share a common 'global' DDI
-    (ie are sourced from different .zarr archives). Think really hard about if
-    that even means anything before doing so.
+    (ie are sourced from different .zarr archives). Think really hard about if a
+    join would even mean anything before doing so.
 
+    Warnings
+    --------
     DDIs are separated by spectral window and correlation (polarity) because it is a
     good indicator of how data is gathered in hardware. Ultimately, if source data
     comes from different DDIs, it means that the data followed different paths
@@ -61,39 +65,55 @@ def ddijoin(xds1, xds2):
     don't haphazardly join DDIs, because it could break the inherent link between
     data and hardware.
 
+
     Notes
     -----
-    *Conflicts in data variable values between datasets:*
+    **Conflicts in data variable values between datasets:**
+
     There are many ways that data values could end up differing between datasets for
     the same coordinate values. One example is the error bars represented in SIGMA
     or WEIGHT could differ for a different spw.
-    There are many possible solutions to dealing with conflicting data values:
-    A) only allow joins that don't have conflicts (current solution)
-    B) add extra indexes CHAN, POL, and/or SPW to the data variables that conflict
-    C) add extra indexes CHAN, POL, and/or SPW to all data variables
-    D) numerically merge the values (average, max, min, etc)
-    E) override the values in xds1 with the values in xds2
 
-    *Current Limitations:*
-    Joins are allowed for datasets that have all different dimension values.
-        Example: xds1 covers time range 22:00-22:59, and xds2 covers time range 23:00-24:00
-    Joins are allowed for datasets that have overlapping dimension values with matching data values at all of those coordinates.
-        Example: xds1.PROCESSOR_ID[0][0] == xds2.PROCESSOR_ID[0][0]
+    There are many possible solutions to dealing with conflicting data values:
+
+    1. only allow joins that don't have conflicts (current solution)
+    2. add extra indexes CHAN, POL, and/or SPW to the data variables that conflict
+    3. add extra indexes CHAN, POL, and/or SPW to all data variables
+    4. numerically merge the values (average, max, min, etc)
+    5. override the values in xds1 with the values in xds2
+
+    **Joins are allowed for:**
+
+    Datasets that have all different dimension values.
+
+    - Example: xds1 covers time range 22:00-22:59, and xds2 covers time range 23:00-24:00
+
+    Datasets that have overlapping dimension values with matching data values at all of those coordinates.
+
+    - Example: xds1.PROCESSOR_ID[0][0] == xds2.PROCESSOR_ID[0][0]
+
+
+    **Current Limitations:**
+
     Joins are not allowed for datasets that have overlapping dimension values with mismatched data values at any of those coordinates.
-        Example: xds1.PROCESSOR_ID[0][0] != xds2.PROCESSOR_ID[0][0]
-        See "Conflicts in data variable values", above
+
+    - Example: xds1.PROCESSOR_ID[0][0] != xds2.PROCESSOR_ID[0][0]
+    - See "Conflicts in data variable values", above
+
     Joins between 'global' datasets, such as those returned by cngi.dio.read_vis(ddi='global'), are probably meaningless and should be avoided.
     Datasets do not need to have the same shape.
-        Example xds1.DATA.shape != xds2.DATA.shape
+
+    - Example xds1.DATA.shape != xds2.DATA.shape
+
 
     Examples
     --------
-    ### Use cases (some of them), to be turned into examples:
-    ### Note: these use cases come from CASA's mstransform(combinespws=True) and may not apply to ddijoin.
-    # universal calibration across spws
-    # autoflagging with broadband rfi
-    # uvcontfit and uvcontsub
-    # joining datasets that had previously been split, operated on, and are now being re-joined
+    ### Use cases (some of them), to be turned into examples. Note: these use cases come from CASA's mstransform(combinespws=True) and may not apply to ddijoin.
+
+    - universal calibration across spws
+    - autoflagging with broadband rfi
+    - uvcontfit and uvcontsub
+    - joining datasets that had previously been split, operated on, and are now being re-joined
     """
 
     import xarray as xr
