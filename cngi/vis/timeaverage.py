004dae60c207d163440e1f5ae77134bb97416cd7
Ryan Raba <ryanraba@gmail.com>
2020-12-10 18:54:09 -0500

    updated to handle new vis format

--- cngi/vis/timeaverage.py
+++ cngi/vis/timeaverage.py
@@ -41,7 +41,7 @@ def timeaverage(xds, width=1, timespan='state', timebin=None):
 
     # function to be mapped out over time groups
     def group_average(gxds):
-
+        
         # function to be mapped out over variables in a group
         def var_average(xda):
             if ('time' in xda.dims) and (xda.dtype.type != np.str_) and (xda.dtype.type != np.bool_):
@@ -49,20 +49,24 @@ def timeaverage(xds, width=1, timespan='state', timebin=None):
             elif 'time' in xda.dims:
                 xda = xda.coarsen(time=width, boundary='pad').max()
             return xda
-
+        
         nxds = gxds.map(var_average, keep_attrs=False)
+        
+        # MSv3 changes this formula
+        #if ('DATA' in gxds.data_vars) and ('SIGMA_SPECTRUM' in gxds.data_vars):
+        #    xda = (gxds.DATA * gxds.SIGMA_SPECTRUM ** 2).coarsen(time=width, boundary='pad').sum()
+        #    nxds['DATA'] = xda / (gxds.SIGMA_SPECTRUM ** 2).coarsen(time=width, boundary='pad').sum()
+        #elif ('DATA' in gxds.data_vars) and ('SIGMA' in gxds.data_vars):
+        #    xda = (gxds.DATA * gxds.SIGMA ** 2).coarsen(time=width, boundary='pad').sum()
+        #    nxds['DATA'] = xda / (gxds.SIGMA ** 2).coarsen(time=width, boundary='pad').sum()
+        if ('DATA' in gxds.data_vars) and ('WEIGHT' in gxds.data_vars):
+            xda = (gxds.DATA / gxds.WEIGHT**0.5).coarsen(time=width, boundary='pad').sum()
+            nxds['DATA'] = xda * (gxds.WEIGHT**0.5).coarsen(time=width, boundary='pad').sum()
 
-        if ('DATA' in gxds.data_vars) and ('SIGMA_SPECTRUM' in gxds.data_vars):
-            xda = (gxds.DATA * gxds.SIGMA_SPECTRUM ** 2).coarsen(time=width, boundary='pad').sum()
-            nxds['DATA'] = xda / (gxds.SIGMA_SPECTRUM ** 2).coarsen(time=width, boundary='pad').sum()
-        elif ('DATA' in gxds.data_vars) and ('SIGMA' in gxds.data_vars):
-            xda = (gxds.DATA * gxds.SIGMA ** 2).coarsen(time=width, boundary='pad').sum()
-            nxds['DATA'] = xda / (gxds.SIGMA ** 2).coarsen(time=width, boundary='pad').sum()
-
-        if ('CORRECTED_DATA' in gxds.data_vars) and ('WEIGHT_SPECTRUM' in gxds.data_vars):
-            xda = (gxds.CORRECTED_DATA * gxds.WEIGHT_SPECTRUM).coarsen(time=width, boundary='pad').sum()
-            nxds['CORRECTED_DATA'] = xda / gxds.WEIGHT_SPECTRUM.coarsen(time=width, boundary='pad').sum()
-        elif ('CORRECTED_DATA' in gxds.data_vars) and ('WEIGHT' in gxds.data_vars):
+        #if ('CORRECTED_DATA' in gxds.data_vars) and ('WEIGHT_SPECTRUM' in gxds.data_vars):
+        #    xda = (gxds.CORRECTED_DATA * gxds.WEIGHT_SPECTRUM).coarsen(time=width, boundary='pad').sum()
+        #    nxds['CORRECTED_DATA'] = xda / gxds.WEIGHT_SPECTRUM.coarsen(time=width, boundary='pad').sum()
+        if ('CORRECTED_DATA' in gxds.data_vars) and ('WEIGHT' in gxds.data_vars):
             xda = (gxds.CORRECTED_DATA * gxds.WEIGHT).coarsen(time=width, boundary='pad').sum()
             nxds['CORRECTED_DATA'] = xda / gxds.WEIGHT.coarsen(time=width, boundary='pad').sum()
 
@@ -76,11 +80,11 @@ def timeaverage(xds, width=1, timespan='state', timebin=None):
     nxds = nxds.reset_coords(notime_coords, drop=True)
 
     if (timespan == 'none') or (timespan is None):
-        cgps = [group_average(sgp[1]) for gp in nxds.groupby('scan') for sgp in gp[1].groupby('state')]
+        cgps = [group_average(sgp[1].unstack('stacked_time_baseline')) for gp in nxds.groupby('SCAN_NUMBER') for sgp in gp[1].groupby('STATE_ID')]
     elif timespan == 'scan':  # span across scans by separating out states
-        cgps = [group_average(gp[1]) for gp in nxds.groupby('state')]
+        cgps = [group_average(gp[1].unstack('stacked_time_baseline')) for gp in nxds.groupby('STATE_ID')]
     elif timespan == 'state':  # span across state by separating out scans
-        cgps = [group_average(gp[1]) for gp in nxds.groupby('scan')]
+        cgps = [group_average(gp[1].unstack('stacked_time_baseline')) for gp in nxds.groupby('SCAN_NUMBER')]
     else:  # span across both
         cgps = [group_average(nxds)]
 
