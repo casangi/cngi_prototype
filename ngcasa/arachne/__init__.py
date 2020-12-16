b1c496d1a5f03343795e46de61f1f133ffba1c99
Jan-Willem Steeb <jsteeb@nrao.edu>
2020-12-16 18:49:14 -0500

    Update __init__.py

--- ngcasa/arachne/__init__.py
+++ ngcasa/arachne/__init__.py
@@ -1,5 +1,13 @@
 """
-Imaging subpackage modules
+Subpackage modules for the Skunk Works program to develop advanced imaging capabilities for ngCASA.
+Functionality in development:
+1. Full Mueller Imaging 
+2. Zernicke Polynomial Aperture Models
+3. Widefield (W projection) Imaging
+4. Wideband Imaging
+5. Heterogeneous Dish Support 
+6. Heterogeneous Pointing Corrections
+7. Heterogeneous Beam Squint Correction
 """
 from .make_gridding_convolution_function import make_gridding_convolution_function
 
