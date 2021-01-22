3953bb74f796eae557424a9fdce006583b7df681
Ryan Raba <ryanraba@gmail.com>
2021-01-22 17:29:31 -0500

    initial version

---
+++ tests/test_ms_conversion.py
@@ -0,0 +1,17 @@
+# requires a data/refimager directory
+# obtain from https://drive.google.com/file/d/1LfEtb5POmAS3Qv-CZ1_01RNtPCltTAMD/view?usp=sharing
+
+import os
+from cngi.conversion import convert_ms
+
+ms_list = [ms for ms in os.listdir('data/refimager') if ms.endswith('.ms')]
+
+for ms in ms_list:
+    print('##### Converting %s #####' % ms.split('/')[-1])
+    mxds = convert_ms('data/refimager/'+ms)
+    
+
+#mxds = convert_ms('data/sis14_twhya_calibrated_flagged.ms')
+#mxds = convert_ms('data/M100_full.ms')
+
+mxds = convert_ms('data/uid___A002_Xc3032e_X27c3.ms', ddis=[0,1,'global'])
