74033a8dd707608142d94f112ee1caebac5b7277
FedeMPouzols <fpouzols@eso.org>
2021-02-21 16:09:35 +0100

    fix  __init__ list

--- ngcasa/flagging/__init__.py
+++ ngcasa/flagging/__init__.py
@@ -2,22 +2,21 @@
 Flagging subpackage modules
 
 These functions can be used to calculate flags and handle different flag versions.
-There are functions to inspect flag variables and manage the set of flag variables in a
-CNGI Dataset. Other functions calculate flags using different flagging methods.
+There are functions to inspect flag variables and manage the set of flag variables
+in a CNGI Dataset. Other functions calculate flags using different flagging
+methods.
 """
-# from .elevation import elevation
-# from .extend import extend
 from .auto_clip import auto_clip
 from .auto_rflag import auto_rflag
 from .auto_tfcrop import auto_tfcrop
 from .auto_uvbin import auto_uvbin
-from .manual_flag import manual_flag
-from .manual_unflag import manual_unflag
-# from .quack import quack
-# from .shadow import shadow
-from .summary import summary
-#from .manage_flags import manage_flags
+from .elevation import elevation
+from .extend import extend
 from .manager_add import manager_add
 from .manager_list import manager_list
 from .manager_remove import manager_remove
-#from .flagmanager_list import flagmanager_list
+from .manual_flag import manual_flag
+from .manual_unflag import manual_unflag
+from .quack import quack
+from .shadow import shadow
+from .summary import summary
