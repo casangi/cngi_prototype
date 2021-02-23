563040db38685f13f82ea4e05840da78f6d69728
FedeMPouzols <fpouzols@eso.org>
2021-02-23 08:32:02 +0100

    move parse_sel_cmd to util file

---
+++ ngcasa/flagging/_flagging_utils/_parse_sel_cmd.py
@@ -0,0 +1,42 @@
+# C
+import numpy as np
+from ._antenna_to_baseline import _antenna_to_baseline
+
+def _parse_sel_cmd(mxds, xds, cmd):
+    """
+    Turn a 'manual' selection command into a dictionary that can be passed to
+    xarray's sel(). This is just a basic implementation for demonstration purposes.
+
+    Parameters
+    ----------
+    cmd: dict
+        Selection dictionay as given to manual_flag
+    mxds: xarray.core.dataset.Dataset
+        Input dataset of datasets
+    xds: xarray.core.dataset.Dataset
+        Input dataset on which flagging will be applied
+    Returns:
+    -------
+    dict
+        Dictionary filtered and adapted to be used with sel() type of selections
+    """
+    result = {}
+    # accept str end/stop and convert to datetime if needed
+    if 'time' in cmd and isinstance(cmd['time'], slice):
+        start, stop = cmd['time'].start, cmd['time'].stop
+        if isinstance(start, str):
+            start = np.datetime64(start)
+        if isinstance(stop, str):
+            stop = np.datetime64(stop)
+        result['time'] = slice(start, stop)
+
+    if 'antenna' in cmd:
+        result['baseline'] = _antenna_to_baseline(mxds, xds, cmd['antenna'])
+
+    if 'chan' in cmd:
+        result['chan'] = chan
+
+    if 'pol'in cmd:
+        result['pol'] = pol
+
+    return result
--- ngcasa/flagging/manual_flag.py
+++ ngcasa/flagging/manual_flag.py
@@ -2,12 +2,10 @@
 """
 this module will be included in the api
 """
-import numpy as np
 import xarray as xr
-import copy
 # Use xdsio.vis_xds_packager to return an mxds?
 # import cngi._helper.io as xdsio
-from ._flagging_utils._antenna_to_baseline import _antenna_to_baseline
+from ._flagging_utils._parse_sel_cmd import _parse_sel_cmd
 
 
 def manual_flag(mxds, xds_idx, commands=None, cmd_file=None):
@@ -39,20 +37,20 @@ def manual_flag(mxds, xds_idx, commands=None, cmd_file=None):
         Dataset with flags set on the selections given in either commands or
         cmd_file
     """
-    xds = mxds.attrs['xds' + '{}'.format(xds_idx)]
     if commands and cmd_file:
         raise RuntimeError('Use either commands (to give a list of commands),'
                            'or cmd_file (to load the list of commands from a '
                            'file)')
+    if not isinstance(commands, list):
+        raise ValueError('Parameter selection must be a list of selection dictionaries')
+    xds = mxds.attrs['xds' + '{}'.format(xds_idx)]
     return _manual_with_reindex_like(mxds, xds, commands)
 
 
 def _manual_with_reindex_like(mxds, xds, cmds):
-    commands = copy.deepcopy(cmds)
     flag_var = 'FLAG'
-
     ret_xds = xds.assign()
-    for cmd in commands:
+    for cmd in cmds:
         selection = _parse_sel_cmd(mxds, xds, cmd)
         fsel = xds[flag_var].sel(selection)
         if 0 in fsel.shape:
@@ -66,43 +64,3 @@ def _manual_with_reindex_like(mxds, xds, cmds):
         ret_xds[flag_var] |= reindexed_slice
 
     return ret_xds
-
-
-def _parse_sel_cmd(mxds, xds, cmd):
-    """
-    Turn a 'manual' selection command into a dictionary that can be passed to
-    xarray's sel(). This is just a basic implementation for demonstration purposes.
-
-    Parameters
-    ----------
-    cmd: dict
-        Selection dictionay as given to manual_flag
-    mxds: xarray.core.dataset.Dataset
-        Input dataset of datasets
-    xds: xarray.core.dataset.Dataset
-        Input dataset on which flagging will be applied
-    Returns:
-    -------
-    dict
-        Dictionary filtered and adapted to be used with sel() type of selections
-    """
-    result = {}
-    # accept str end/stop and convert to datetime if needed
-    if 'time' in cmd and isinstance(cmd['time'], slice):
-        start, stop = cmd['time'].start, cmd['time'].stop
-        if isinstance(start, str):
-            start = np.datetime64(start)
-        if isinstance(stop, str):
-            stop = np.datetime64(stop)
-        result['time'] = slice(start, stop)
-
-    if 'antenna' in cmd:
-        result['baseline'] = _antenna_to_baseline(mxds, xds, cmd['antenna'])
-
-    if 'chan' in cmd:
-        result['chan'] = chan
-
-    if 'pol'in cmd:
-        result['pol'] = pol
-    print(' *** Going for cmd: {}, selection: {}'.format(cmd, result))
-    return result
