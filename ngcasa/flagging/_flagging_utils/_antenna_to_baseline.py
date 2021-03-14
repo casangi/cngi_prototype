3764be98e0e3f446a2248aaf72d3eaa9292a3b62
Federico Montesino Pouzols <fpouzols@eso.org>
2021-03-14 17:44:06 +0100

    add license headers

--- ngcasa/flagging/_flagging_utils/_antenna_to_baseline.py
+++ ngcasa/flagging/_flagging_utils/_antenna_to_baseline.py
@@ -1,4 +1,17 @@
-# C
+#   Copyright 2020-21 European Southern Observatory, ALMA partnership
+#   Copyright 2020 AUI, Inc. Washington DC, USA
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 import numpy as np
 
 
@@ -18,4 +31,5 @@ def _antenna_to_baseline(mxds, xds, ant_name):
 
     ant_idx = ant_name_to_idx(mxds, ant_name)
     baseline_idxs = ant_idx_to_baseline_idxs(xds, ant_idx)
+
     return baseline_idxs
