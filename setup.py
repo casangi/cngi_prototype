68e303e92f017fd90c5eb7b4a757c2d50ee8f70c
Andrew Thomas <amcnicho@nrao.edu>
2021-03-19 13:16:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', "r") as fid:   #encoding='utf-8'
 
 setup(
     name='cngi_prototype',
-    version='0.0.78',
+    version='0.0.79',
     description='CASA Next Generation Infrastructure Prototype',
     long_description=long_description,
     long_description_content_type="text/markdown",
